14ded142cc28affeca141960cf931e7b1c40f1c4
jjvee <realwogus@gmail.com>
2020-03-26 13:16:15 +0900

    Update settings.py

--- salesweb/salesweb/settings.py
+++ salesweb/salesweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^h4n3#tknix3c7shcn7dlkz1_i2)ls3f_-erzg(2d(8!j*!y#6'
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
